846205358af3beadc63524997289ac0783a47a27
Capitan <miqueasguaba77@gmail.com>
2024-07-20 20:21:24 -0400

    Adición de la funcionalidad para desencriptar un texto

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function encriptar() {
     let tituloMensaje = document.getElementById("titulo_mensaje");
     let parrafo = document.getElementById("parrafo");
     let caricatura = document.getElementById("caricatura");
+
     let textoCifrado = texto
         .replace(/e/gi, "enter")
         .replace(/i/gi, "imes")
@@ -20,6 +21,35 @@ function encriptar() {
         parrafo.textContent = "";
         caricatura.src = "./imagenes/encriptado.jpg";
     }else{
+        caricatura.src = "./imagenes/Muñeco.png";
+        alert("Debes ingresar algún texto");
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado"
+    }
+}
+
+function desencriptar () {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo_mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let caricatura = document.getElementById("caricatura");
+    let textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u")
+        .replace(/cr/gi, "c")
+        .replace(/trat/gi, "m")
+        .replace(/-m/gi, "s")
+        .replace(/mkr/gi, "g")
+
+    if (texto.length !=0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        caricatura.src = "./imagenes/desencriptado.jpg";
+    } else {
         caricatura.src = "./imagenes/Muñeco.png";
         alert("Debes ingresar algún texto");
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
